9384b8fc0fa43d8c7bc031bcb24717fb5d2b470d
Nathan Hill <pjsstp@gmail.com>
2020-03-19 10:03:59 -0400

    added SQL database connection and controller files and set up connection for local DB integration during development

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,14 @@
+const db = require("../controller")
+
 
 module.exports = app => {
 
     app.get('/express_backend', (req, res) => {
         res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
       });
+
+    app.post('/get_zip', (req, res) => {
+
+    } )
 
 }
