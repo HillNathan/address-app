ac9fe20b99b96cb55b3de35c98f975538d635860
Nathan Hill <pjsstp@gmail.com>
2020-03-20 14:20:41 -0400

    added main get route to routes module

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,9 @@ const db = require("../controller")
 
 module.exports = app => {
 
-    // app.get('/', (req,res) => {
-    //     res.send('index.html')
-    // })
+    app.get('/', (req,res) => {
+        res.send('index.html')
+    })
 
     app.get('/express_backend', (req, res) => {
         res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
