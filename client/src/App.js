962c69ca7d6ba06ce7ca282797af7eabc4986efd
Nathan Hill <pjsstp@gmail.com>
2020-03-20 11:02:53 -0400

    added react-router-dom to handle non-db related get calls

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,10 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from "react-router-dom";
 import './App.css';
 import ZipCodeEntry from './components/ZipCodeEntry'
 import AddressEntry from './components/AddressEntry'
@@ -97,57 +103,61 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1>Address</h1>
-        </header>
-        <div className="container">
-          <div className="row">
-            <div className="col">
-              <ZipCodeEntry 
-                userZip = {this.state.userZip}
-                handleChange = {this.handleChange}
-              />
-            </div>
-          </div>
-
-          <div className="row">
-            <div className="col">
-              <div className="message">
-                {this.state.status}
-              </div>
-            </div>
-          </div>
-
-          <div className="row">
-            <div className="col">
-              {this.state.showAddress ? 
-                <div> 
-                  {this.state.city + ", " + this.state.state}
-                </div> : 
-                <div></div>}
-            </div>
-          </div>
-
-          <div className="row">
-            <div className="col">
-              {this.state.showAddress ? 
-                <div> 
-                  <AddressEntry 
-                    address = {this.state.address}
-                    city = {this.state.city}
-                    state = {this.state.state}
-                    zip = {this.state.zip}
+      <Router>
+        <Route exact path="/">
+          <div className="App">
+            <header className="App-header">
+              <h1>Address</h1>
+            </header>
+            <div className="container">
+              <div className="row">
+                <div className="col">
+                  <ZipCodeEntry 
+                    userZip = {this.state.userZip}
                     handleChange = {this.handleChange}
-                    handleClick = {this.callMapIt}
                   />
-                </div> : 
-                <div></div>}
+                </div>
+              </div>
+
+              <div className="row">
+                <div className="col">
+                  <div className="message">
+                    {this.state.status}
+                  </div>
+                </div>
+              </div>
+
+              <div className="row">
+                <div className="col">
+                  {this.state.showAddress ? 
+                    <div> 
+                      {this.state.city + ", " + this.state.state}
+                    </div> : 
+                    <div></div>}
+                </div>
+              </div>
+
+              <div className="row">
+                <div className="col">
+                  {this.state.showAddress ? 
+                    <div> 
+                      <AddressEntry 
+                        address = {this.state.address}
+                        city = {this.state.city}
+                        state = {this.state.state}
+                        zip = {this.state.zip}
+                        handleChange = {this.handleChange}
+                        handleClick = {this.callMapIt}
+                      />
+                    </div> : 
+                    <div></div>}
+                </div>
+              </div>
+
             </div>
           </div>
-
-        </div>
-      </div>
+          </Route>
+      </Router>
     );
   }
 }
