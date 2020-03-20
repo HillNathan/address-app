1a0ae5534fa77fa948d4c7ff15c4a39aa46192e1
Nathan Hill <pjsstp@gmail.com>
2020-03-20 15:20:49 -0400

    deploying

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,7 @@ import React, { Component } from 'react';
 import {
   BrowserRouter as Router,
   Route,
-  Switch,
-  Redirect
+  Switch
 } from "react-router-dom";
 import './App.css';
 import ZipCodeEntry from './components/ZipCodeEntry'
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
 const db = require("../controller");
-var path = require("path");
+// var path = require("path");
 
 module.exports = app => {
 
-    app.get('/', (req,res) => {
-        res.sendFile('path.join("index.html")');
-    })
+    // app.get('/', (req,res) => {
+    //     res.sendFile('path.join("index.html")');
+    // })
 
     app.get('/express_backend', (req, res) => {
         res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
