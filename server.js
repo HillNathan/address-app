de2fcbe0b0ea02182546cc9a6d12359adba49cb9
Nathan Hill <pjsstp@gmail.com>
2020-03-23 14:23:09 -0400

    building app

--- server.js
+++ server.js
@@ -13,3 +13,4 @@ app.listen(port, () => console.log(`Listening on port ${port}`));
 app.get('/express_backend', (req, res) => {
   res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
 });
+
