a9cba51a3ad2c6c180a8125364f8a4cbe6625f8d
Nathan Hill <pjsstp@gmail.com>
2020-03-23 16:08:04 -0400

    building app

--- routes/index.js
+++ routes/index.js
@@ -11,16 +11,14 @@ module.exports = app => {
         res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
       });
 
-    app.get('/get_home', (req, res) => {
-        db.getInfo(17603, response => {
-            res.send({data: response})
-            }
-        )
-    });
-
     app.post('/get_zip', (req, res) => {
         db.getInfo(req.body.zipCode, response => {
-            res.send({data: response})
+            if (err) {
+                res.send({data:err})
+            }
+            else {
+                res.send({data: response})
+            }
             }
         )    
     });
